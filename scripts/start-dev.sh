4780da169cf7831451121e01dcd78cf617014f63
phuctm97 <phuctm97@gmail.com>
2019-09-08 17:58:52 +0700

    Write custom Dockerfile that supports VSCode Remote-Containers and necessary Go extensions

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+# Exit immediately on failure.
+set -e
+
+# Find absolute path of local workspace based on script's path.
 local_workdir=$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) >/dev/null 2>&1 && pwd)
 
 main() {
+  local image_name=go-restful-dev
+  local container_name=go-restful-dev
   local container_workdir=/go/src/github.com/the-evengers/go-restful
-  local container_name=go-restful
 
+  # Build image (if necessary).
+  docker build --rm -t $image_name $local_workdir
+
+  # Start container.
   docker run --rm -it \
     --name $container_name \
     --volume $local_workdir:$container_workdir \
     --workdir $container_workdir \
-    golang
+    $image_name
 }
 
 main
+
