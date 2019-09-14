e7e4b20e86d1c88a086d7cef35c7b8a48ac60594
phuctm97 <phuctm97@gmail.com>
2019-09-14 19:32:36 +0700

    PoC HTTP views

--- main.go
+++ main.go
@@ -4,19 +4,40 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"runtime"
 	"github.com/gorilla/mux"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	os := runtime.GOOS
-	fmt.Fprintf(w, "Hello World from [%s] container!\n", os)
+func listUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List users\n")
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Get user %s\n", vars["userId"])
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create user\n")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Update user %s\n", vars["userId"])
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Delete user %s\n", vars["userId"])
 }
 
 func main() {
 	// Configure routes.
 	router := mux.NewRouter()
-	router.HandleFunc("/", handle)
+	router.HandleFunc("/users/", listUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/", createUser).Methods(http.MethodPost)
+	router.HandleFunc("/users/{userId}/", getUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{userId}/", updateUser).Methods(http.MethodPut)
+	router.HandleFunc("/users/{userId}/", deleteUser).Methods(http.MethodDelete)
 
 	// Start HTTP server.
 	if err := http.ListenAndServe(":8000", router); err != nil {
