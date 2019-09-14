78a2da9feb83720b8d92af04a57eeca0698ed7d4
Minh-Phuc Tran <phuctm97@gmail.com>
2019-09-14 19:34:25 +0700

    Implement simple HTTP views as PoC (#20)
    * Add PoC HTTP views to main.go

--- main.go
+++ main.go
@@ -4,17 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"runtime"
+	"github.com/gorilla/mux"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	os := runtime.GOOS
-	fmt.Fprintf(w, "Hello World from [%s] container!\n", os)
+func listUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List users\n")
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Get user %s\n", vars["userId"])
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create user\n")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Update user %s\n", vars["userId"])
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Delete user %s\n", vars["userId"])
 }
 
 func main() {
-	http.HandleFunc("/", handle)
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	// Configure routes.
+	router := mux.NewRouter()
+	router.HandleFunc("/users/", listUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/", createUser).Methods(http.MethodPost)
+	router.HandleFunc("/users/{userId}/", getUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{userId}/", updateUser).Methods(http.MethodPut)
+	router.HandleFunc("/users/{userId}/", deleteUser).Methods(http.MethodDelete)
+
+	// Start HTTP server.
+	if err := http.ListenAndServe(":8000", router); err != nil {
 		log.Fatal(err)
 	}
 }
