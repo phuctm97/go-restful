ecff97a8e4331ef455e86bb1048020df8c178b56
phuctm97 <phuctm97@gmail.com>
2019-09-24 22:01:59 +0700

    Add user entities

---
+++ common/errors.go
@@ -0,0 +1,14 @@
+package common
+
+import "fmt"
+
+// Errors is a collection of errors.
+type Errors []error
+
+func (errs Errors) Error() string {
+	msgs := make([]string, len(errs))
+	for i, err := range errs {
+		msgs[i] = err.Error()
+	}
+	return fmt.Sprintf("%#v\n", msgs)
+}
