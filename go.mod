0d690cf769a7c92d859e4cc07e9a96d7636fa6d5
phuctm97 <phuctm97@gmail.com>
2019-09-27 22:50:52 +0700

    Add users entities unit tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/the-evengers/go-restful
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/stretchr/testify v1.4.0
+)
---
+++ users/errors.go
@@ -0,0 +1,16 @@
+package users
+
+import "errors"
+
+// Errors.
+var (
+	ErrUsernameIsTooShort            = errors.New("username must be at least 1-character length")
+	ErrUsernameIsTooLong             = errors.New("username must be at most 32-character length")
+	ErrUsernameContainsInvalidChar   = errors.New("username must contain only digits, underscores, dashes, dots and alphabetical letters")
+	ErrUsernameBeginsWithInvalidChar = errors.New("username must begin with either underscore or an alphabetical letter")
+	ErrEmailIsInvalid                = errors.New("email is invalid")
+	ErrEmailIsTooLong                = errors.New("email must be at most 128-character length")
+	ErrFullNameIsTooShort            = errors.New("full name must be at least 1-character length")
+	ErrFullNameIsTooLong             = errors.New("full name must be at most 128-character length")
+	ErrBioIsTooLong                  = errors.New("bio must be at most 128-character length")
+)
