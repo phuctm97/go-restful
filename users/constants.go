b4eeddb004d9a4e584f3a468e3139379ab1d100a
phuctm97 <phuctm97@gmail.com>
2019-09-30 23:12:16 +0700

    Add validate user unique

--- users/constants.go
+++ users/constants.go
@@ -1,15 +1,19 @@
 package users
 
-import "errors"
+import (
+	"errors"
+)
 
 // Errors.
 var (
 	ErrUsernameIsTooShort            = errors.New("username must be at least 1-character length")
 	ErrUsernameIsTooLong             = errors.New("username must be at most 32-character length")
+	ErrUsernameWasUsed               = errors.New("username was used")
 	ErrUsernameContainsInvalidChar   = errors.New("username must contain only digits, underscores, dashes, dots and alphabetical letters")
 	ErrUsernameBeginsWithInvalidChar = errors.New("username must begin with either underscore or an alphabetical letter")
 	ErrEmailIsInvalid                = errors.New("email is invalid")
 	ErrEmailIsTooLong                = errors.New("email must be at most 128-character length")
+	ErrEmailWasUsed                  = errors.New("email was used")
 	ErrFullNameIsTooShort            = errors.New("full name must be at least 1-character length")
 	ErrFullNameIsTooLong             = errors.New("full name must be at most 128-character length")
 	ErrBioIsTooLong                  = errors.New("bio must be at most 128-character length")
